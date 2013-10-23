438961a172fe5ce8ed98e99bbe7de4a44b570bb9
dannylsl <dannylsl@hotmail.com>
2013-10-23 09:30:37 +0800

    python practice

---
+++ pythonCode/001/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2.7
+
+print "########### RAW Input ##########"
+username = raw_input("What is your name?")
+print "hello,"+username
+
+print "########### Input ##########"
+username = input("What is your name? ")
+print "hello,"+username
