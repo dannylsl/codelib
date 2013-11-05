278363da10ba370522baf44467a6630c21d8e199
dannylsl <dannylsl@hotmail.com>
2013-11-05 21:58:10 +0800

    excel read with xlrd and write with xlwt failed copy style

--- xls_analyze/excel.py
+++ xls_analyze/excel.py
@@ -1,7 +1,23 @@
 import xlrd
-data = xlrd.open_workbook('MRFLD_SOCWATCH_ANALYSIS_PR2_TEMPLATE.xlsx')
+import xlwt
+
+from xlutils.copy import copy
+
+data = xlrd.open_workbook('MRFLD_SOCWATCH_ANALYSIS_PR2_TEMPLATE.xlsx', formatting_info = True)
 
 table = data.sheets()[0]
 
-cell_B4 = table.cell(1,3).value
+cell_B4 = table.cell(3,1)
+cell_B4_value = table.cell(3,1).value
+
 print cell_B4
+print cell_B4_value
+
+table.put_cell(3, 1, 2, 0.115, 0)
+cell_B4 = table.cell(3,1)
+print cell_B4  #xlrd can read data from xlsx, but failed to write data to xlsx
+
+wb = copy(data)
+ws = wb.get_sheet(0)
+
+wb.save("out.xlsx")
