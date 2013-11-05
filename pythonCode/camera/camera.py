50b225444c60201fef1cccd391104ebd828a88e2
dannylsl <dannylsl@hotmail.com>
2013-11-05 18:07:24 +0800

    python take a capture from camera

--- pythonCode/camera/camera.py
+++ pythonCode/camera/camera.py
@@ -5,12 +5,15 @@ from pygame.locals import *
 pygame.init()
 pygame.camera.init()
 
-window = pygame.display.set_mode((640,480),0)
-cam = pygame.camera.Camera("/dev/video0", (640,480))
+window = pygame.display.set_mode((320,240),0)
+cam = pygame.camera.Camera("/dev/video0", (320,240))
 cam.start()
+##image = cam.get_image()
+##pygame.image.save(image,'abc.jpg')
+##cam.stop()
 while True:
 	image = cam.get_image()
-	#pygame.image.save(image,'abc.jpg')
+	pygame.image.save(image,'abc.bmp')
 	window.blit(image,(0,0))
 	pygame.display.flip()
 cam.stop()
