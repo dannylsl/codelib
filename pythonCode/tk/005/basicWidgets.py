dd726e41122a3296c4004759c1e0746f2e0ce2c9
dannylsl <dannylsl@hotmail.com>
2013-11-28 22:52:00 +0800

    list: Table of content basicWidget.py: basic widgets of python Tkinter

--- pythonCode/tk/005/basicWidgets.py
+++ pythonCode/tk/005/basicWidgets.py
@@ -4,14 +4,27 @@ from Tkinter import *
 import ttk
 
 root = Tk()
+root.title("Basic Widgets In python Tkinter")
+
 frame = ttk.Frame(root)
 
 frame['padding'] = (5,10)
 frame['borderwidth'] = 2
 frame['relief'] = 'sunken'  # flat (default) | raised | sunken | solid | ridge | groove
 
+#frame.columnconfigure(0, weight = 12)
+#frame.rowconfigure(0, weight = 1)
+
 frame.grid(column = 0, row = 0, sticky = (N, W, E, S))
 
-l1 = ttk.Label(frame, text='Python Tkinter Basic Widget: Label')
+l1 = ttk.Label(frame, text='Python Tkinter Basic Widget: Label').grid(column=1, row=1)
+
+resultsContents = StringVar()
+l2 = ttk.Label(frame, textvariable = resultsContents).grid(column = 3, row = 3)
+resultsContents.set('New value to display')
+
+button = ttk.Button(frame, text='Okay').grid(column = 2, row = 2)
+
+
 
 root.mainloop()
