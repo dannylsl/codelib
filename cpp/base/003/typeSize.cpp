ee7701ff316981117d143cb4ff13bfd77a749f89
dannylsl <dannylsl@hotmail.com>
2013-12-01 22:08:25 +0800

    Built-in type size check

---
+++ cpp/base/003/typeSize.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout<<"Type char size ="<< sizeof(char) <<endl;		
+	cout<<"Type int size ="<< sizeof(int) <<endl;		
+	cout<<"Type short size ="<< sizeof(short) <<endl;		
+	cout<<"Type long size ="<< sizeof(long) <<endl;		
+	cout<<"Type long long size ="<< sizeof(long long) <<endl;		
+	cout<<"===========================================" <<endl;
+	cout<<"Type float size ="<< sizeof(float) <<endl;		
+	cout<<"Type double size ="<< sizeof(double) <<endl;		
+
+	return 0;
+}
+
