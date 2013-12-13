874d69e7af8a0264c2993821c3dfe7c0c0e8bead
dannylsl <dannylsl@hotmail.com>
2013-12-13 18:15:57 +0800

    python __new__ return None && __init__ Exception

--- pythonCode/basic/004/foo.py
+++ pythonCode/basic/004/foo.py
@@ -1,5 +1,12 @@
 #!/bin/python
+
+# Python class Exception process
+# 1. RETRUN NONE IN __new__ IF PARAMETER CHECK RETURN FALSE
+# 2. RAISE EXCEPTION IN __init__ IF PARAMETER CHECK RETURN FALSE
+# 3. RAISE EXCEPTION IN __new__ IF PARAMETER CHECK RETURN FALSE
+
 import os
+import time
 
 class Foo(object):
 #    file = ""
@@ -24,17 +31,33 @@ class Foo(object):
         print "**" * 10
 
     def __del__(self) :
-        print ">>>>>>>>>> __del__ called"
+        print "FOO __del__ called"
 
 
 class Foo2(object) :
 ### raise Exception while failed to initialize instance
     def __init__(self, file) :
+        print "Foo2 __init__ called"
         if os.path.exists(file) == False :
-            raise RuntimeError
-        # TO BE DONE
+            raise ValueError("File not exist while Foo2")
+
+    def __del__(self) :
+        print "FOO2 __del__ called"
 
 
+
+class Foo3(object) :
+    def __new__(cls, file) :
+        print "Foo3 __new__ called"
+        if os.path.exists(file) == False :
+            raise ValueError("File not exist while Foo3")
+        print "Foo3 __new__ called END"
+
+    def __init__(self, file) :
+        print "Foo3 __init__ called"
+
+    def __del__(self) :
+        print "FOO3 __del__ called"
 
 
 def print_obj(obj) :
@@ -44,6 +67,7 @@ def print_obj(obj) :
         print obj
         #print repr(obj)
 
+
 if __name__ == '__main__' :
 
     foo = Foo("ttyUSB01111111")
@@ -52,3 +76,18 @@ if __name__ == '__main__' :
     print "==" * 10
     foo1 = Foo("example")
     print_obj(foo1)
+
+    print "11111" * 10
+    try :
+        foo2 = Foo2("aaa")
+    except ValueError,e:
+#       print_obj(foo2) # FAILED foo2 not exist
+        print "ValueError:",e
+
+    print "22222" * 10
+    try :
+        foo3 = Foo3("bbb")
+    except ValueError,e:
+        print "ValueError:",e
+
+    print "Program End"
