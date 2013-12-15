3c2c2d638df7b7d430c1434ab9e6e7cf6fb63efa
dannylsl <dannylsl@hotmail.com>
2013-12-16 07:33:55 +0800

    change tabs into spaces

--- cpp/base/003/typeSize.cpp
+++ cpp/base/003/typeSize.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main(void) {
-	cout<<"Type char size ="<< sizeof(char) <<endl;		
-	cout<<"Type int size ="<< sizeof(int) <<endl;		
-	cout<<"Type short size ="<< sizeof(short) <<endl;		
-	cout<<"Type long size ="<< sizeof(long) <<endl;		
-	cout<<"Type long long size ="<< sizeof(long long) <<endl;		
-	cout<<"===========================================" <<endl;
-	cout<<"Type float size ="<< sizeof(float) <<endl;		
-	cout<<"Type double size ="<< sizeof(double) <<endl;		
+    cout<<"Type char size ="<< sizeof(char) <<endl;
+    cout<<"Type int size ="<< sizeof(int) <<endl;
+    cout<<"Type short size ="<< sizeof(short) <<endl;
+    cout<<"Type long size ="<< sizeof(long) <<endl;
+    cout<<"Type long long size ="<< sizeof(long long) <<endl;
+    cout<<"===========================================" <<endl;
+    cout<<"Type float size ="<< sizeof(float) <<endl;
+    cout<<"Type double size ="<< sizeof(double) <<endl;
 
-	return 0;
+    return 0;
 }
 
--- shell/setupwizard.sh
+++ shell/setupwizard.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 adb root
 sleep 2
 adb remount
