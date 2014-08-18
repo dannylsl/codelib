e0aba263d662248217347737ef280f3234656fd9
dannylsl <dannylsl@hotmail.com>
2014-08-18 21:25:34 +0800

    CountSorting3 on  hackerrank

--- cPro/hackerrank/countingSort1/main.c
+++ cPro/hackerrank/countingSort1/main.c
@@ -7,36 +7,37 @@
 
 
 void print_arr(int ar_size, short int ar[]) {
-	int i ;
-	for(i = 0; i < ar_size; i++) {
-		printf("%d ", ar[i]);	
-	};
-	return;
+    int i ;
+    for(i = 0; i < ar_size; i++) {
+        printf("%d ", ar[i]);
+    };
+    return;
 }
+
 
 void print_countSorted_data(int ar_size, short int ar[]) {
-	int i, j;
-	for(i = 0; i < ar_size; i++) {
-		for(j = 0; j < ar[i]; j++) {
-			printf("%d ",i);	
-		}	
-	}
+    int i, j;
+    for(i = 0; i < ar_size; i++) {
+        for(j = 0; j < ar[i]; j++) {
+            printf("%d ",i);
+        }
+    }
 }
 
-int main(int argc, char *argv[]) {	
-	int len;
-	short int count[100] = {0};
-	int i,tmp;
+int main(int argc, char *argv[]) {
+    int len;
+    short int count[100] = {0};
+    int i,tmp;
 
-	//printf("Input the array length:");
-	scanf("%d",&len);
+    //printf("Input the array length:");
+    scanf("%d",&len);
 
-	for(i = 0; i < len; i++) {
-		scanf("%d",&tmp);	
-		count[tmp]++;
-	}
-//	print_arr(100, count);
-	print_countSorted_data(100, count);	
+    for(i = 0; i < len; i++) {
+        scanf("%d",&tmp);
+        count[tmp]++;
+    }
+    //  print_arr(100, count);
+    print_countSorted_data(100, count);
 
-	return 0;
+    return 0;
 }
