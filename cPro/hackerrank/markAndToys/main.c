1a1d302fd9c081ff49363e2a5dd6d488ac63dbc6
dannylsl <dannylsl@hotmail.com>
2014-08-21 17:37:16 +0800

    Use quickSort on Mark and Toy

--- cPro/hackerrank/markAndToys/main.c
+++ cPro/hackerrank/markAndToys/main.c
@@ -1,6 +1,5 @@
 //https://www.hackerrank.com/challenges/mark-and-toys
 
-
 #include <stdio.h>
 #include <malloc.h>
 
@@ -27,6 +26,43 @@ void insertionSort(int ar_size, int *  ar) {
         ar[available] = value;
     }
 }
+
+void quickSort(int ar_size, int *  ar) {
+    int l_ar[ar_size], r_ar[ar_size];
+    int l_ar_len = 0, r_ar_len = 0;
+    int value = ar[0];
+    int i;
+    for(i = 1; i < ar_size; i++) {
+        if( ar[i] <= value ) {
+            l_ar[l_ar_len] = ar[i];
+            l_ar_len++;
+        }else if(ar[i] > value) {
+            r_ar[r_ar_len] = ar[i];
+            r_ar_len++;
+        }
+    }
+
+    if(l_ar_len > 1) {
+        quickSort(l_ar_len, l_ar);
+    }
+    if(r_ar_len > 1) {
+        quickSort(r_ar_len, r_ar);
+    }
+
+    // Combine Array
+    for(i = 0; i < ar_size; i++) {
+        if( i < l_ar_len) {
+            ar[i] = l_ar[i];
+        }else if(i == l_ar_len) {
+            ar[i] = value;
+        }else {
+            ar[i] = r_ar[i - l_ar_len - 1];
+        }
+    }
+//    print_ar(ar_size, ar);
+}
+
+
 
 int get_max_buy( int ar_size, int * ar, int money) {
     int i;
@@ -59,7 +95,8 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    insertionSort(j, toys);
+ //   insertionSort(j, toys);
+    quickSort(j, toys);
     count = get_max_buy(j, toys, K);
     printf("%d",count);
     return 0;
