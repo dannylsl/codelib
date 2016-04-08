60a981914440c88cb006537a4cc168da4e95597f
dannylsl <dannylsl@hotmail.com>
2016-04-08 10:07:45 +0800

    JavaScript Avalon learning

---
+++ javascript/avalon/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TODO</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <script src="avalon.modern.js"></script>
+    </head>
+    <body>
+        <div>TODO write content</div>
+    </body>
+</html>
